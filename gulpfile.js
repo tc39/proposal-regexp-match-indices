fb8a53736fa074a2601c1507457735d37c9cc956
Ron Buckton <ron.buckton@microsoft.com>
2019-07-26 14:29:22 -0700

    Update to build scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 const del = require("del");
 const gulp = require("gulp");
 const emu = require("gulp-emu");
-const gls = require("gulp-live-server");
+const livereload = require("gulp-livereload");
+const http = require("http");
+const st = require("st");
 
 const clean = () => del("docs/**/*");
 gulp.task("clean", clean);
@@ -9,18 +11,26 @@ gulp.task("clean", clean);
 const build = () => gulp
     .src(["spec/index.html"])
     .pipe(emu())
-    .pipe(gulp.dest("docs"));
+    .pipe(gulp.dest("docs"))
+    .pipe(livereload());
 gulp.task("build", build);
 
-const watch = () => gulp
-    .watch(["spec/**/*"], build);
+const watch = () => {
+    livereload.listen({ basePath: "docs" });
+    return gulp.watch(["spec/**/*"], build);
+};
 gulp.task("watch", watch);
 
-const start = () => {
-    const server = gls.static("docs", 8080);
-    const promise = server.start();
-    gulp.watch(["docs/**/*"], file => server.notify(file));
-    return promise;
+const start = (done) => {
+    http.createServer(st({ 
+        path: __dirname + '/docs',
+        index: 'index.html',
+        cache: false
+    })).listen(8080, e => {
+        if (e) return done(e);
+        console.log(`folder "docs" serving at http://localhost:8080`);
+        done();
+    });
 };
 gulp.task("start", gulp.parallel(watch, start));
 
