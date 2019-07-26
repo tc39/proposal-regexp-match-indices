1a818d2dee59b7e2c6dd4e52b1f73092fc6154ca
Ron Buckton <ron.buckton@microsoft.com>
2019-07-26 15:04:50 -0700

    Build changes to support azure pipelines

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+/// <reference types="node" />
+const path = require("path");
 const del = require("del");
 const gulp = require("gulp");
 const emu = require("gulp-emu");
@@ -5,30 +7,32 @@ const livereload = require("gulp-livereload");
 const http = require("http");
 const st = require("st");
 
-const clean = () => del("docs/**/*");
+const DOCS_DIR = process.env.DOCS_DIR || "docs";
+
+const clean = () => del(`${DOCS_DIR}/**/*`);
 gulp.task("clean", clean);
 
 const build = () => gulp
     .src(["spec/index.html"])
     .pipe(emu())
-    .pipe(gulp.dest("docs"))
+    .pipe(gulp.dest(DOCS_DIR))
     .pipe(livereload());
 gulp.task("build", build);
 
 const watch = () => {
-    livereload.listen({ basePath: "docs" });
+    livereload.listen({ basePath: DOCS_DIR });
     return gulp.watch(["spec/**/*"], build);
 };
 gulp.task("watch", watch);
 
 const start = (done) => {
     http.createServer(st({ 
-        path: __dirname + '/docs',
+        path: path.resolve(DOCS_DIR),
         index: 'index.html',
         cache: false
     })).listen(8080, e => {
         if (e) return done(e);
-        console.log(`folder "docs" serving at http://localhost:8080`);
+        console.log(`folder "${DOCS_DIR}" serving at http://localhost:8080`);
         done();
     });
 };
