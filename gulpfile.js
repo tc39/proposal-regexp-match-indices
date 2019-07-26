3e1fabe6df6a04bc38552f857182dde65ac69498
Ron Buckton <ron.buckton@microsoft.com>
2019-07-26 15:44:44 -0700

    Remove output folder customization

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,3 @@
-/// <reference types="node" />
-const path = require("path");
 const del = require("del");
 const gulp = require("gulp");
 const emu = require("gulp-emu");
@@ -7,35 +5,30 @@ const livereload = require("gulp-livereload");
 const http = require("http");
 const st = require("st");
 
-const DOCS_DIR = process.env.DOCS_DIR || "docs";
-if (DOCS_DIR !== "docs") {
-    console.log(`DOCS_DIR set to '${DOCS_DIR}'`);
-}
-
-const clean = () => del(`${DOCS_DIR}/**/*`);
+const clean = () => del("docs/**/*");
 gulp.task("clean", clean);
 
 const build = () => gulp
     .src(["spec/index.html"])
     .pipe(emu())
-    .pipe(gulp.dest(DOCS_DIR))
+    .pipe(gulp.dest("docs"))
     .pipe(livereload());
 gulp.task("build", build);
 
 const watch = () => {
-    livereload.listen({ basePath: DOCS_DIR });
+    livereload.listen({ basePath: "docs" });
     return gulp.watch(["spec/**/*"], build);
 };
 gulp.task("watch", watch);
 
 const start = (done) => {
     http.createServer(st({ 
-        path: path.resolve(DOCS_DIR),
+        path: __dirname + '/docs',
         index: 'index.html',
         cache: false
     })).listen(8080, e => {
         if (e) return done(e);
-        console.log(`folder "${DOCS_DIR}" serving at http://localhost:8080`);
+        console.log(`folder "docs" serving at http://localhost:8080`);
         done();
     });
 };
